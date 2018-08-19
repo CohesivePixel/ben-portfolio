bad28c543eabf94a6c27ae5027262020c441fa3f
Ben-Ey <logins@ben-ey.nl>
2018-08-19 15:32:23 +0200

    got the structure for the mobile backplate ready

--- resources/assets/js/components/mobile-backplate.vue
+++ resources/assets/js/components/mobile-backplate.vue
@@ -1,5 +1,12 @@
 <template lang="html">
+  <div class="mbpl container">
+    <div class="mbpl flow">
 
+    </div>
+    <div class="mbpl static">
+
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,5 +17,28 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style lang="scss">
+  .mbpl {
+    width: 100%;
+    border: 1px solid;
+  }
+  .container {
+    position: absolute;
+    top: 50vh;
+    height: 200px;
+    display: flex;
+    flex-direction: column-reverse;
+    flex-wrap: wrap;
+  }
+  .static {
+    flex: 1;
+    flex-grow: 0;
+    flex-shrink: 0;
+    flex-basis: 10vw;
+  }
+  .flow {
+    flex: 1;
+    flex-grow: 1;
+    flex-shrink: 1;
+  }
 </style>
